c254ac797ee3c70fcd67d1b7b94e99150d1c90fc
SimJH99 <78871184+SimJH99@users.noreply.github.com>
2023-12-07 09:45:58 +0900

    [level unrated] Title: 369게임, Time: 0.03 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120891. 369게임/369게임.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int order) {
+        int answer = 0;
+        
+        String str = Integer.toString(order);
+        
+        for(int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if(ch == '3' || ch == '6' || ch == '9') {
+                answer++;
+            }
+        }
+        
+        
+        return answer;
+    }
+}
