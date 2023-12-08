2282c3bc9f4706b7a64fad69fed837fe019d3d13
SimJH99 <78871184+SimJH99@users.noreply.github.com>
2023-12-08 09:27:23 +0900

    [level unrated] Title: 문자열로 변환, Time: 0.04 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181845. 문자열로 변환/문자열로 변환.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(int n) {
+        String answer = new String();
+        
+        answer = Integer.toString(n);
+        
+        
+        return answer;
+    }
+}
