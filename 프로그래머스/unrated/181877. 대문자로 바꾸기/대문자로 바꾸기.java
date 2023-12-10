4ae8a5aafd4e73c0973d5f2dc58a56615d977a63
SimJH99 <78871184+SimJH99@users.noreply.github.com>
2023-12-10 13:49:58 +0900

    [level unrated] Title: 대문자로 바꾸기, Time: 0.02 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181877. 대문자로 바꾸기/대문자로 바꾸기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = new String();
+        
+        answer = myString.toUpperCase();
+        
+        return answer;
+    }
+}
