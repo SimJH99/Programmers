91a6879f0fad9863b8b3de638960135ee4446eed
SimJH99 <78871184+SimJH99@users.noreply.github.com>
2023-12-13 11:41:26 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 1.81 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -1,22 +1,38 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] numbers) {
-         List<Integer> num_list = new ArrayList<>();
-
-
-        for(int i =0; i < numbers.length-1; i++){
+        Set<Integer> myTreeSet = new TreeSet<>();
+        
+        for(int i =0; i<numbers.length-1; i++){
             for(int j = i+1; j < numbers.length; j++){
-                if(!num_list.contains(numbers[i]+numbers[j]))
-                num_list.add(numbers[i] + numbers[j]);
+                myTreeSet.add(numbers[i] + numbers[j]);
             }
         }
-
-        Collections.sort(num_list);
-        int[] answer = new int[num_list.size()];
-        for(int i = 0; i< num_list.size(); i++){
-            answer[i] = num_list.get(i);
-        }
         
+        int[] answer = new int[myTreeSet.size()];
+        int count = 0;
+        for(int a : myTreeSet){
+            answer[count] = a;
+            count++;
+        }
         return answer;
+        
+        
+//          List<Integer> num_list = new ArrayList<>();
+
+//         for(int i =0; i < numbers.length-1; i++){
+//             for(int j = i+1; j < numbers.length; j++){
+//                 if(!num_list.contains(numbers[i]+numbers[j]))
+//                 num_list.add(numbers[i] + numbers[j]);
+//             }
+//         }
+
+//         Collections.sort(num_list);
+//         int[] answer = new int[num_list.size()];
+//         for(int i = 0; i< num_list.size(); i++){
+//             answer[i] = num_list.get(i);
+        // }
+        
+        // return answer;
     }
 }
