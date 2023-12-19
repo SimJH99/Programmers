c4fa4f03e25d8faaf7299f71447cc956654a69e7
SimJH99 <78871184+SimJH99@users.noreply.github.com>
2023-12-19 19:07:19 +0900

    [level unrated] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.19 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        
+        String[] result = my_string.split("[A-Za-z]");
+
+        for(int i =0; i < result.length; i++){
+           if(!result[i].isEmpty()){
+               answer += Integer.parseInt(result[i]);
+           }
+        }
+        
+        return answer;
+    }
+}
